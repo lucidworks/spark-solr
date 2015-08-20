82a06b6d9459e409e7c1565565b51530b9254d70
Ganesh Kumar <ganeshkumar@Ganeshs-MacBook-Pro.local>
2015-08-20 13:44:02 -0700

    unit test

--- src/test/java/com/lucidworks/spark/SolrRelationTest.java
+++ src/test/java/com/lucidworks/spark/SolrRelationTest.java
@@ -1,12 +1,20 @@
 package com.lucidworks.spark;
 
-import org.apache.spark.sql.DataFrame;
-import org.apache.spark.sql.SQLContext;
-import org.apache.spark.sql.SaveMode;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.*;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
 import org.junit.Test;
+import scala.Predef;
+import scala.Tuple2;
+import scala.collection.JavaConverters;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.Assert.assertTrue;
@@ -90,6 +98,59 @@ public class SolrRelationTest extends RDDProcessorTestBase {
 
     deleteCollection(testCollection);
     deleteCollection("testFilterSupport2");
+  }
+
+
+  @Test
+  public void testNestedDataFrames() throws Exception {
+    SQLContext sqlContext = new SQLContext(jsc);
+    String confName = "testConfig";
+    File confDir = new File("src/test/resources/conf");
+    int numShards = 2;
+    int replicationFactor = 1;
+    deleteCollection("testNestedDataFrames");
+    createCollection("testNestedDataFrames", numShards, replicationFactor, 2, confName, confDir);
+    List<StructField> fields = new ArrayList<StructField>();
+    List<StructField> fields1 = new ArrayList<StructField>();
+    List<StructField> fields2 = new ArrayList<StructField>();
+    fields.add(DataTypes.createStructField("id", DataTypes.StringType, true));
+    fields1.add(DataTypes.createStructField("test1_s", DataTypes.StringType, true));
+    fields1.add(DataTypes.createStructField("test2_s", DataTypes.StringType, true));
+    fields2.add(DataTypes.createStructField("test11_s", DataTypes.StringType, true));
+    fields2.add(DataTypes.createStructField("test12_s", DataTypes.StringType, true));
+    fields1.add(DataTypes.createStructField("testtype_s", DataTypes.createStructType(fields2) , true));
+    fields.add(DataTypes.createStructField("test_s", DataTypes.createStructType(fields1), true));
+    StructType schema = DataTypes.createStructType(fields);
+    Row dm = RowFactory.create("7", RowFactory.create("test1", "test2", RowFactory.create("test11", "test12")));
+    List<Row> list = new ArrayList<Row>();
+    list.add(dm);
+    //JavaSparkContext sc = new JavaSparkContext(sqlContext.sparkContext().conf());
+    JavaRDD<Row> rdd = jsc.parallelize(list);
+    DataFrame df = sqlContext.createDataFrame(rdd, schema);
+    java.util.Map<String,String> config = new HashMap<String,String>();
+    String zkHost = cluster.getZkServer().getZkAddress();
+    config.put("zkhost", zkHost);
+    config.put("collection", "testNestedDataFrames");
+    scala.collection.immutable.Map<String,String> config1 = JavaConverters.mapAsScalaMapConverter(config).asScala().toMap(Predef.<Tuple2<String, String>>conforms());
+    try {
+          SolrRelation sr = new SolrRelation(sqlContext, config1, df);
+          df.show();
+          df.printSchema();
+          java.util.HashMap<String,Object> uniqueID = new HashMap<String,Object>();
+          uniqueID.put("__lwcontenttype_s", "metadata");
+          sr.sendToSolr(sr.convertToSolrDocuments(df, uniqueID));
+          Thread.sleep(1000);
+          SolrRDD srd = new SolrRDD(zkHost,"testNestedDataFrames");
+          java.util.HashMap<String,Object> queryInput = new HashMap<String,Object>();
+          queryInput.put("__lwroot_s", "root");
+          queryInput.put("__lwcontenttype_s", "metadata");
+          DataFrame reconstructed = srd.readDataFrame(jsc, sqlContext, queryInput);
+          reconstructed.show();
+          reconstructed.printSchema();
+          deleteCollection("testNestedDataFrames");
+    } catch (Exception e) {
+          e.printStackTrace();
+    }
   }
 
   protected void assertCount(long expected, long actual, String expr) {
