348130b6620dad16eb22f8640049424777355dd0
kiranchitturi <kiran.chitturi@lucidworks.com>
2016-02-26 09:29:37 -0800

    Add tests

--- src/test/scala/com/lucidworks/spark/EventsimTestSuite.scala
+++ src/test/scala/com/lucidworks/spark/EventsimTestSuite.scala
@@ -5,16 +5,31 @@ import org.apache.spark.Logging
 
 class EventsimTestSuite extends EventsimBuilder with Logging {
 
-  test("Query using RDD") {
+  test("Simple Query using RDD") {
     val solrRDD = new SolrRDD(zkHost, collectionName, sc)
       .query("*:*")
       .rows(10)
       .select(Array("id"))
+    testCommons(solrRDD)
+  }
 
+  test("Split partitions default") {
+    val solrRDD = new SolrRDD(zkHost, collectionName, sc).doSplits()
+    testCommons(solrRDD)
+  }
+
+  test("Split partitions by field name") {
+    val solrRDD = new SolrRDD(zkHost, collectionName, sc).splitField("id").splitsPerShard(2)
+    testCommons(solrRDD)
+  }
+
+  def testCommons(solrRDD: SolrRDD): Unit = {
     val sparkCount = solrRDD.count()
 
+    assert(solrRDD.getNumPartitions == numShards)
+    // assert counts
     assert(sparkCount == solrRDD.solrCount.toLong)
     assert(sparkCount == eventSimCount)
-
   }
+
 }
