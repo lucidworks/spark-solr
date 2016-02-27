4cb5ba381e1a562359e184ae762c6868e010b0b6
kiranchitturi <kiran.chitturi@lucidworks.com>
2016-02-26 16:00:54 -0800

    more tests

--- src/test/scala/com/lucidworks/spark/EventsimTestSuite.scala
+++ src/test/scala/com/lucidworks/spark/EventsimTestSuite.scala
@@ -1,9 +1,10 @@
 package com.lucidworks.spark
 
 import com.lucidworks.spark.rdd.SolrRDD
-import org.apache.spark.Logging
+import com.lucidworks.spark.util.ConfigurationConstants
+import org.apache.spark.sql.DataFrame
 
-class EventsimTestSuite extends EventsimBuilder with Logging {
+class EventsimTestSuite extends EventsimBuilder {
 
   test("Simple Query using RDD") {
     val solrRDD = new SolrRDD(zkHost, collectionName, sc)
@@ -24,6 +25,39 @@ class EventsimTestSuite extends EventsimBuilder with Logging {
     testCommons(solrRDD)
   }
 
+  test("SQL query") {
+    val df: DataFrame = sqlContext.read.format("solr").option("zkHost", zkHost).option("collection", collectionName).load()
+    assert(df.count() == eventSimCount)
+  }
+
+  test("SQL query splits") {
+    val options = Map(
+      "zkHost" -> zkHost,
+      "collection" -> collectionName,
+      ConfigurationConstants.SOLR_DO_SPLITS -> "true"
+    )
+    val df: DataFrame = sqlContext.read.format("solr").options(options).load()
+    assert(df.rdd.getNumPartitions > numShards)
+  }
+
+  test("SQL query no params should produce IllegalArgumentException") {
+    intercept[IllegalArgumentException] {
+      sqlContext.read.format("solr").load()
+    }
+  }
+
+  test("SQL query no zkHost should produce IllegalArgumentException") {
+    intercept[IllegalArgumentException] {
+      sqlContext.read.format("solr").option("zkHost", zkHost).load()
+    }
+  }
+
+  test("SQL query no collection should produce IllegalArgumentException") {
+    intercept[IllegalArgumentException] {
+      sqlContext.read.format("solr").option("collection", collectionName).load()
+    }
+  }
+
   def testCommons(solrRDD: SolrRDD): Unit = {
     val sparkCount = solrRDD.count()
 
@@ -31,5 +65,6 @@ class EventsimTestSuite extends EventsimBuilder with Logging {
     assert(sparkCount == solrRDD.solrCount.toLong)
     assert(sparkCount == eventSimCount)
   }
+
 
 }
