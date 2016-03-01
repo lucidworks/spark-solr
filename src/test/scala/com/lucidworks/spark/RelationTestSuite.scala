52426c44c58e7137b9dadb788a25129d5c8d7176
kiranchitturi <kiran.chitturi@lucidworks.com>
2016-03-01 10:15:30 -0800

    Warn about unknown parameters passed to Sql query

---
+++ src/test/scala/com/lucidworks/spark/RelationTestSuite.scala
@@ -0,0 +1,15 @@
+package com.lucidworks.spark
+
+import com.lucidworks.spark.util.ConfigurationConstants._
+
+class RelationTestSuite extends SparkSolrFunSuite {
+
+  test("Unknown params") {
+    val paramsToCheck = Set(SOLR_ZK_HOST_PARAM, SOLR_COLLECTION_PARAM, SOLR_QUERY_PARAM, ESCAPE_FIELDNAMES_PARAM, "fl", "q", "solr.defType")
+    val unknownParams = SolrRelation.checkUnknownParams(paramsToCheck)
+    assert(unknownParams.size == 2)
+    assert(unknownParams("q"))
+    assert(unknownParams("fl"))
+  }
+
+}
