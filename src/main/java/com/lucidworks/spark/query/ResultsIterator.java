b87f5ac0b54c86ad5d1b1aa0ec10645772e81b25
kiranchitturi <kiran.chitturi@lucidworks.com>
2016-03-27 09:23:28 -0700

    Abstract out query result Iterators
    * Log the number of rows fetched from each partition

---
+++ src/main/java/com/lucidworks/spark/query/ResultsIterator.java
@@ -0,0 +1,11 @@
+package com.lucidworks.spark.query;
+
+import org.apache.solr.common.SolrDocument;
+
+import java.util.Iterator;
+
+public abstract class ResultsIterator implements Iterator<SolrDocument>, Iterable<SolrDocument>{
+
+  public abstract long getNumDocs();
+
+}
