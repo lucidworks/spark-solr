39f6a8b6a626114147344bc961a9c5c0877fabca
Steve Rowe <sarowe@gmail.com>
2016-04-07 12:22:33 -0400

    MLPipelineScala: add CLI Option for default collection (ml20news)

--- src/main/scala/com/lucidworks/spark/example/ml/MLPipelineScala.scala
+++ src/main/scala/com/lucidworks/spark/example/ml/MLPipelineScala.scala
@@ -45,7 +45,9 @@ class MLPipelineScala extends SparkApp.RDDProcessor {
     OptionBuilder().longOpt("classifier").hasArg.argName("TYPE").required(false).desc(
       s"Classifier type: either NaiveBayes or LogisticRegression. Default: $DefaultClassifier").build(),
     OptionBuilder().longOpt("sample").hasArg.argName("FRACTION").required(false).desc(
-      s"Fraction (0 to 1) of full dataset to sample from Solr. Default: $DefaultSample").build())
+      s"Fraction (0 to 1) of full dataset to sample from Solr. Default: $DefaultSample").build(),
+    OptionBuilder().longOpt("collection").hasArg.argName("NAME").required(false)
+      .desc("Solr source collection; default: $DefaultCollection").build())
 
   override def run(conf: SparkConf, cli: CommandLine): Int = {
     val jsc = new SparkContext(conf)
