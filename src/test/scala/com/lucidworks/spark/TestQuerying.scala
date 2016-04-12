d13683ce6bd1ed2e6918f820d04b4f4774860c39
kiranchitturi <kiran.chitturi@lucidworks.com>
2016-04-12 16:38:36 -0700

    Do not set fields if everything from schema is requested. Added a test

--- src/test/scala/com/lucidworks/spark/TestQuerying.scala
+++ src/test/scala/com/lucidworks/spark/TestQuerying.scala
@@ -37,4 +37,35 @@ class TestQuerying extends TestSuiteBuilder {
       SolrCloudUtil.deleteCollection(collectionName, cluster)
     }
   }
+
+  test("vary queried columns with fields option") {
+    val collectionName = "testQuerying-" + UUID.randomUUID().toString
+    SolrCloudUtil.buildCollection(zkHost, collectionName, null, 2, cloudClient, sc)
+    try {
+      val csvFileLocation = "src/test/resources/test-data/simple.csv"
+      val csvDF = sqlContext.read.format("com.databricks.spark.csv")
+        .option("header", "true")
+        .option("inferSchema", "true")
+        .load(csvFileLocation)
+      assert(csvDF.count == 3)
+
+      val solrOpts = Map("zkhost" -> zkHost, "collection" -> collectionName, "fields" -> "id,one_txt,two_txt")
+      csvDF.write.format("solr").options(solrOpts).mode(Overwrite).save()
+
+      // Explicit commit to make sure all docs are visible
+      val solrCloudClient = SolrSupport.getCachedCloudClient(zkHost)
+      solrCloudClient.commit(collectionName, true, true)
+
+      val solrDF = sqlContext.read.format("solr").options(solrOpts).load()
+      assert(solrDF.count == 3)
+      assert(solrDF.schema.fields.length === 3)
+      val oneColFirstRow = solrDF.select("one_txt").head()(0) // query for one column
+      assert(oneColFirstRow != null)
+      val firstRow = solrDF.head.toSeq                        // query for all columns
+      assert(firstRow.size === 3)
+      firstRow.foreach(col => assert(col != null))            // no missing values
+    } finally {
+      SolrCloudUtil.deleteCollection(collectionName, cluster)
+    }
+  }
 }
