d7b65dbe83d1fd9df15932361b25fa865aa4224f
kiranchitturi <kiran.chitturi@lucidworks.com>
2016-05-08 22:22:28 -0700

    Upgrade to Solr 5.5.1

--- src/main/java/com/lucidworks/spark/util/EmbeddedSolrServerFactory.java
+++ src/main/java/com/lucidworks/spark/util/EmbeddedSolrServerFactory.java
@@ -2,6 +2,7 @@ package com.lucidworks.spark.util;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -93,9 +94,7 @@ public class EmbeddedSolrServerFactory implements Serializable {
     CoreContainer coreContainer = new CoreContainer(solrResourceLoader);
     coreContainer.load();
 
-    CoreDescriptor descr =
-      new CoreDescriptor(coreContainer, coreName, instanceDir.getAbsolutePath());
-    SolrCore core = coreContainer.create(descr);
+    SolrCore core = coreContainer.create(coreName, instanceDir.toPath(), Collections.<String, String>emptyMap());
     return new EmbeddedSolrServer(coreContainer, coreName);
   }
 
