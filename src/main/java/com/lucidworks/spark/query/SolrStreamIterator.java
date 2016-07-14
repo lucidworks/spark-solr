1a337731a32921b5af39102f6498bae4f448664b
Timothy Potter <thelabdude@gmail.com>
2016-07-14 09:46:08 -0600

    Refactor SolrStreamIterator to extend from TupleStreamIterator

--- src/main/java/com/lucidworks/spark/query/SolrStreamIterator.java
+++ src/main/java/com/lucidworks/spark/query/SolrStreamIterator.java
@@ -6,16 +6,13 @@ import org.apache.log4j.Logger;
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.impl.CloudSolrClient;
-import org.apache.solr.client.solrj.io.Tuple;
 import org.apache.solr.client.solrj.io.stream.SolrStream;
-import org.apache.solr.common.SolrDocument;
+import org.apache.solr.client.solrj.io.stream.TupleStream;
 import org.apache.solr.common.params.CommonParams;
 
 import java.io.IOException;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.NoSuchElementException;
 
 /**
  * An iterator over a stream of query results from one Solr Core. It is a
@@ -24,21 +21,19 @@ import java.util.NoSuchElementException;
  * This iterator is not thread safe. It is intended to be used within the
  * context of a single thread.
  */
-public class SolrStreamIterator extends ResultsIterator {
+public class SolrStreamIterator extends TupleStreamIterator {
 
   private static final Logger log = Logger.getLogger(SolrStreamIterator.class);
 
-  private SolrStream stream;
   protected SolrClient solrServer;
   protected SolrQuery solrQuery;
-  protected boolean closeAfterIterating;
-  protected long numDocs = 0;
-  private boolean isOpenStream;
-  private SolrDocument currentTuple = null;
+  protected String shardUrl;
 
   public SolrStreamIterator(String shardUrl, SolrClient solrServer, SolrQuery solrQuery) {
+    super(solrQuery);
+
+    this.shardUrl = shardUrl;
     this.solrServer = solrServer;
-    this.closeAfterIterating = !(solrServer instanceof CloudSolrClient);
     this.solrQuery = solrQuery;
 
     if (solrQuery.getRequestHandler() == null) {
@@ -46,8 +41,24 @@ public class SolrStreamIterator extends ResultsIterator {
     }
     solrQuery.setRows(null);
     SolrQuerySupport.validateExportHandlerQuery(solrServer, solrQuery);
-    this.stream = new SolrStream(shardUrl, solrQuery);
-    openStream();
+  }
+
+  protected TupleStream openStream() {
+    SolrStream stream;
+    try {
+      stream = new SolrStream(shardUrl, solrQuery);
+      stream.open();
+      // stream.setTrace(true);
+    } catch (IOException e1) {
+      throw new RuntimeException(e1);
+    }
+    return stream;
+  }
+
+  protected void afterStreamClosed() throws Exception {
+    if (!(solrServer instanceof CloudSolrClient)) {
+      IOUtils.closeQuietly(solrServer);
+    }
   }
 
   /**
@@ -82,77 +93,5 @@ public class SolrStreamIterator extends ResultsIterator {
       }
     }
     return sink;
-  }
-
-  public synchronized boolean hasNext() {
-    try {
-      if (currentTuple == null) {
-        currentTuple = fetchNextDocument();
-      }
-    } catch (IOException e) {
-      log.error("Failed to fetch next document for query: " + solrQuery.toQueryString(), e);
-      throw new RuntimeException(e);
-    }
-
-    if (currentTuple == null && closeAfterIterating) {
-      // TODO: upstream change to SolrStream to make it AutoCloseable
-      try {
-        stream.close();
-      } catch (IOException e) {
-        log.error("Failed to close the SolrStream.", e);
-        throw new RuntimeException(e);
-      }
-      IOUtils.closeQuietly(solrServer);
-    }
-
-    return currentTuple != null;
-  }
-
-  protected SolrDocument fetchNextDocument() throws IOException {
-    Tuple tuple = stream.read();
-    if (tuple.EOF)
-      return null;
-
-    final SolrDocument doc = new SolrDocument();
-    for (Object key : tuple.fields.keySet()) {
-      doc.setField((String) key, tuple.get(key));
-    }
-
-    return doc;
-  }
-
-  private void openStream() {
-    try {
-      if (isOpenStream)
-        return;
-      stream.open();
-      // stream.setTrace(true);
-      isOpenStream = true;
-    } catch (IOException e1) {
-      throw new RuntimeException(e1);
-    }
-  }
-
-  public synchronized SolrDocument next() {
-    if (currentTuple == null)
-      throw new NoSuchElementException();
-
-    final SolrDocument tempCurrentTuple = currentTuple;
-    currentTuple = null;
-    ++numDocs;
-    return tempCurrentTuple;
-  }
-
-  public void remove() {
-    throw new UnsupportedOperationException("remove is not supported");
-  }
-
-  public Iterator<SolrDocument> iterator() {
-    return this;
-  }
-
-  @Override
-  public long getNumDocs() {
-    return numDocs;
   }
 }
