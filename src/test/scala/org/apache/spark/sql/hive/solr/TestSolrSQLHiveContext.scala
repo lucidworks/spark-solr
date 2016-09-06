e3fbf13273d643673b0dbffea243e526da2589ed
kiranchitturi <kiran.chitturi@lucidworks.com>
2016-09-06 13:25:46 -0700

    Improve test

--- src/test/scala/org/apache/spark/sql/hive/solr/TestSolrSQLHiveContext.scala
+++ src/test/scala/org/apache/spark/sql/hive/solr/TestSolrSQLHiveContext.scala
@@ -22,22 +22,24 @@ class TestSolrSQLHiveContext extends MovielensBuilder {
 
   // end to end usecase testing
   test("Execute SQL query as Solr") {
+    // Filter on movie_id = 9
+    val solrSQLStmt = "SELECT movie_id, COUNT(*) as aggCount FROM movielens_ratings WHERE (rating >= 4 AND movie_id = 9) GROUP BY movie_id ORDER BY aggCount desc"
+    val sqlStmt = "SELECT m.title as title, solr.aggCount as aggCount FROM movies m INNER JOIN (" + solrSQLStmt + ") as SOLR ON solr.movie_id = m.movie_id ORDER BY aggCount DESC"
+    val tempTableName = SolrSQLHiveContext.getTempTableName(solrSQLStmt.toLowerCase, "movielens_ratings")
 
     // Non cached lookup
     {
-      val solrSQLStmt = "SELECT movie_id, COUNT(*) as aggCount FROM movielens_ratings WHERE rating >= 4 GROUP BY movie_id ORDER BY aggCount desc"
-      val sqlStmt = "SELECT m.title as title, solr.aggCount as aggCount FROM movies m INNER JOIN (" + solrSQLStmt + ") as SOLR ON solr.movie_id = m.movie_id ORDER BY aggCount DESC"
-
       // Register movies collection as a table
       val opts = Map("zkhost" -> zkHost, "collection" -> moviesColName)
       sHiveContext.read.format("solr").options(opts).load().registerTempTable("movies")
 
       val df = sHiveContext.sql(sqlStmt)
-      val count = df.count()
-      assert(count==100)
+      val rows = df.collect()
+      assert(rows.length == 1)
+      assert(rows(0)(1) == 23)
 
       // Check if the temp table is created
-      val tempTableName = SolrSQLHiveContext.getTempTableName(solrSQLStmt.toLowerCase, "movielens_ratings")
+
       val tables = sHiveContext.catalog.getTables(None)
       assert(tables.contains(tempTableName, true))
 
@@ -46,6 +48,16 @@ class TestSolrSQLHiveContext extends MovielensBuilder {
 
       // Check if the cached temp table is actually part of the physical plan
       assert(df.logicalPlan.toJSON.contains(tempTableName))
+    }
+
+    // Cached lookup
+    {
+      val df = sHiveContext.sql(sqlStmt)
+      val rows = df.collect()
+      assert(rows.length == 1)
+      assert(rows(0)(1) == 23)
+
+      assert(sHiveContext.cachedSQLQueries.size == 1)
     }
  }
 }
