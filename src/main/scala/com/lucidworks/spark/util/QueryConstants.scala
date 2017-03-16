671bad8f0d23c990cdfc342b7f6eb08beb335752
kiranchitturi <kiran.chitturi@lucidworks.com>
2017-03-16 11:11:46 -0700

    Increase default page size to 5000

--- src/main/scala/com/lucidworks/spark/util/QueryConstants.scala
+++ src/main/scala/com/lucidworks/spark/util/QueryConstants.scala
@@ -8,7 +8,7 @@ object QueryConstants {
   val QT_SELECT = "/select"
 
   val DEFAULT_REQUIRED_FIELD: String = "id"
-  val DEFAULT_PAGE_SIZE: Int = 1000
+  val DEFAULT_PAGE_SIZE: Int = 5000
   val DEFAULT_QUERY: String = "*:*"
   val DEFAULT_SPLITS_PER_SHARD: Int = 10
   val DEFAULT_SPLIT_FIELD: String = "_version_"
